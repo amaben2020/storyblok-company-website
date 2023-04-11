b2016bcf83c221bbe0a3974f68cfbccc26dac372
algomachine007 <amaben202phantom@gmail.com>
2023-04-11 17:45:15 +0100

    fix

--- components/myHero.js
+++ components/myHero.js
@@ -2,7 +2,6 @@ import { storyblokEditable } from "gatsby-source-storyblok";
 import React from "react";
 
 const myHero = ({ blok }) => {
-  console.log({ ...storyblokEditable(blok) });
   return <div {...storyblokEditable(blok)}>{blok.title}</div>;
 };
 
